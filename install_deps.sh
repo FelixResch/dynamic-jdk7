588c67052e0897a47399f5392be1d74f108b9150
felix <resch.felix@gmail.com>
2016-07-11 18:34:00 +0200

    fixed deps

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/FelixResch/jdk7-support.git jdk7-support
+git clone https://github.com/femoio/jdk7-support.git jdk7-support
 cd jdk7-support
 mvn install -Dmaven.javadoc.skip=true -DskipTests=true -B -V
 cd ..
